de7eab97c536b6fcce7c1c02f267177be28d5367
jcassarly <jaredcassarly@case.edu>
2021-01-04 16:25:41 -0500

    removed todo

--- dashboard/views/_positions/_non_ec/_public_relations_chair.py
+++ dashboard/views/_positions/_non_ec/_public_relations_chair.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from dashboard.forms import PhotoForm
 from dashboard.utils import committee_meeting_panel, photo_form, verify_position
 
-# TODO: why was this previously commented out before i did this refactor?
 @verify_position(['Public Relations Chair', 'Recruitment Chair', 'Vice President', 'President', 'Adviser'])
 def public_relations_c(request):
     committee_meetings, context = committee_meeting_panel('Public Relations Chair')
