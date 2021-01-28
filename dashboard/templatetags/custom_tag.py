df915803bebc9f0cab3aff219ce73395898f5788
helper08 <ccpg02@gmail.com>
2021-01-28 00:19:28 -0500

    added docstring for template tag

--- dashboard/templatetags/custom_tag.py
+++ dashboard/templatetags/custom_tag.py
@@ -3,8 +3,19 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def append_ast_if_req(field):
+    """ Adds a new filter to template tags that for use in templates. Used by writing {{ field | append_ast_if_req }}
+        @register registers the filter into the django template library so it can be used in template.
+
+    :param Form.field field:
+        a field of a form that you would like to return the label and potentially an asterisk for.
+    :returns:
+        The field label and, if it's a required field, an asterisk
+    :rtype: string
+
+    """
     if field.field.required:
         return field.label + '*'
     else:
